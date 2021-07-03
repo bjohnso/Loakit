e836e2406f6d3b3ab1e5fd74f3781b016384da38
Brandon Johnson <brandon.johnson@Brandon-Johnson.local>
2021-07-03 22:20:36 +0200

    sponsorship validation

--- contracts/Sponsorship.sol
+++ contracts/Sponsorship.sol
@@ -3,8 +3,10 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Sponsorship {
 
     Sponsor public sponsor; // sponsor
-    uint256 stake; // amount of gwei to stake
-    uint scoreRequirement; // pass mark to validate this contract
+    uint256 public balance; // amount of wei staked in this contract
+    uint public scoreRequirement; // pass mark to validate this contract
+    uint256 public minimumStake = 10000000000000000; // minimum stake for this contract
+    address public selectedSchool;
 
     struct Sponsor {
         string name; // sponsor name
@@ -29,20 +31,55 @@ contract Sponsorship {
     }
 
     constructor(string memory sponsorName) {
+        require(
+            bytes(sponsorName).length > 0,
+            "Sponsor must have a name"
+        );
         sponsor.name = sponsorName;
         sponsor.delegate = msg.sender;
     }
 
     mapping(address => School) public schools;
 
+    function stakeReward(uint score) public payable {
+        require(
+            msg.sender == sponsor.delegate,
+            "Only the sponsor may stake a reward."
+        );
+        require(
+            scoreRequirement == 0,
+            "A reward can not be staked more than once."
+        );
+        require(
+            msg.value >= minimumStake,
+            "A stake can not be less than 0.01 eth."
+        );
+        require(
+            score > 0,
+            "A score can not be 0."
+        );
+
+        balance += msg.value;
+        scoreRequirement = score;
+    }
+
+
     function addSchool(string memory name, string[] memory questions, string[] memory answers) public {
         require(
             msg.sender != sponsor.delegate,
-            "The sponser may not also be a school."
+            "The sponsor may not also be a school."
         );
         require(
             questions.length == answers.length,
             "Every question must have an answer."
+        );
+        require(
+            scoreRequirement > 0,
+            "A score requirement must be specified before a school can be added."
+        );
+        require(
+            balance >= minimumStake,
+            "A stake of at least 0.01 ether must be set before a school can be selected."
         );
 
         Syllabus memory syllabus = Syllabus({
@@ -58,19 +95,36 @@ contract Sponsorship {
         });
     }
 
-    function selectSchool(address schoolAddress) public view returns (address selectedSchool) {
+    function selectSchool(address schoolAddress) public returns (address selected) {
         require(
             msg.sender == sponsor.delegate,
             "Only the sponsor may select a school."
+        );
+        require(
+            selectedSchool == address(0x0),
+            "A school can not be selected when one is already selected."
+        );
+        require(
+            schools[schoolAddress].delegate == schoolAddress,
+            "A school address must exist."
+        );
+        require(
+            scoreRequirement > 0,
+            "A score requirement must be specified before a school can be selected."
+        );
+        require(
+            balance > minimumStake,
+            "A stake of at least 0.01 ether must be set before a school can be selected."
         );
 
         School memory school = schools[schoolAddress];
 
         if (school.delegate == schoolAddress) {
             school.selected = true;
-            selectedSchool = school.delegate;
+            selected = school.delegate;
+            selectedSchool = selected;
         }
 
-        return selectedSchool;
+        return selected;
     }
 }
